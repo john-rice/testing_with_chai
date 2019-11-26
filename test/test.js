5020c15448b6561a4f5727cb69286f8ca427f41c
johnrice <jrice@labact.com>
2019-11-25 20:03:27 -0500

    Provide some chai'licious test samples

---
+++ test/test.js
@@ -0,0 +1,39 @@
+// require testing modules
+const mocha = require('mocha')
+const expect = require('chai').expect
+
+// require the module that we are putting to the test
+// or in this case, copy the snippet below from slack:
+
+// prove all the things
+describe('My comprehension of the JS basics is solid', () => {
+
+    // prove statements that support the "describe"
+    it('and this proves my awesomeness with JS', () => {
+        expect('so far so good').to.be.a('string');
+        expect({}).to.be.an('object');
+        expect(null).to.be.a('null');
+        expect(undefined).to.be.an('undefined');
+        expect([]).to.be.an('array').that.is.empty;
+        expect([5,4,3,2,1]).to.be.an('array').that.includes(5);
+        expect('I am a strong string').to.not.be.an('array')
+        expect({model: 'Toyota'}).to.have.a.property('model');
+        expect({make: 'tesla', model: 'cybertruck'}).to.include({make: 'tesla', model: 'cybertruck'});
+    }) 
+
+    it('includes testing skills', () => {
+        expect(2).not.to.equal('2')
+        expect(Number('2')).not.to.equal('2')
+        expect(2).to.equal(2)
+        expect(Number('two')).to.be.NaN
+        expect(Number('2')).not.to.be.NaN
+        expect(2).not.to.be.NaN
+        expect(Boolean(1)).to.be.true
+        expect(Boolean(0)).to.be.false
+        expect(Boolean('')).to.not.be.true
+        expect(Boolean(undefined)).to.be.false
+        expect({}.foo).to.be.an.undefined
+        expect({foo: 'on you'}.foo).not.to.be.an.undefined
+    }) 
+
+})
